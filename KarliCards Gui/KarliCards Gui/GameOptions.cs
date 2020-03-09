087089dee139d5a73650b67e90cf27b181661666
martinnebesar <nebesar.martin@gmail.com>
2020-03-09 19:06:36 -0400

    Chapter16Uploaded

---
+++ KarliCards Gui/KarliCards Gui/GameOptions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarliCards_Gui
+{
+    [Serializable]
+    public class GameOptions : INotifyPropertyChanged
+    {
+        private bool _playerAgainstComputer = true;
+        private int _numberOfPlayers = 2;
+        private int _minutedBeforeLoss = 10;
+        private ComputerSkillLevel _computerSkill = ComputerSkillLevel.Dumb;
+        private ObservableCollection<string> _playerNames = new ObservableCollection<string>();
+
+        public List<string> SelectedPlayers { get; set; }
+
+        public GameOptions()
+        {
+            SelectedPlayers = new List<string>();
+        }
+        public ObservableCollection<string> PlayerNames
+        {
+            get
+            {
+                return _playerNames;
+            }
+            set
+            {
+                _playerNames = value;
+                OnPropertyChanged("PlayerNames");
+            }
+        }
+        public void AddPlayer(string playerName)
+        {
+            if (_playerNames.Contains(playerName))
+                return;
+            _playerNames.Add(playerName);
+            OnPropertyChanged("PlayerNames");
+        }
+        private bool _playAgainstComputer;
+
+        public int NumberOfPlayers
+        {
+            get { return _numberOfPlayers; }
+            set
+            {
+                _numberOfPlayers = value;
+                OnPropertyChanged("NumberOfPlayers");
+            }
+        }
+        public bool PlayAgainstComputer
+        {
+            get { return _playAgainstComputer; }
+            set
+            {
+                _playerAgainstComputer = value;
+                OnPropertyChanged("PlayAgainstComputer");
+            }
+        }
+        public int MinutesBeforeLoss
+        {
+            get { return _minutedBeforeLoss; }
+            set
+            {
+                _minutedBeforeLoss = value;
+                OnPropertyChanged("MinutesBeforeLoss");
+            }
+        }
+        public ComputerSkillLevel ComputerSkill
+        {
+            get { return _computerSkill; }
+            set
+            {
+                _computerSkill = value;
+                OnPropertyChanged("ComputerSkill");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    [Serializable]
+    public enum ComputerSkillLevel
+    {
+        Dumb,
+        Good,
+        Cheats
+    }
+}
---
+++ KarliCards Gui/KarliCards Gui/StartGame.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace KarliCards_Gui
+{
+    /// <summary>
+    /// Interaction logic for StartGame.xaml
+    /// </summary>
+    public partial class StartGame : Window
+    {
+        private GameOptions _gameOptions;
+        public StartGame()
+        {
+            if (_gameOptions == null)
+            {
+                if (File.Exists("GameOptions.xml"))
+                {
+                    using (var stream = File.OpenRead("GameOptions.xml"))
+                    {
+                        var serializer = new XmlSerializer(typeof(GameOptions));
+                        _gameOptions = serializer.Deserialize(stream) as GameOptions;
+                    }
+                }
+                else
+                    _gameOptions = new GameOptions();
+            }
+            DataContext = _gameOptions;
+            InitializeComponent();
+            if (_gameOptions.PlayAgainstComputer)
+                playerNamesListBox.SelectionMode = SelectionMode.Single;
+            else
+                playerNamesListBox.SelectionMode = SelectionMode.Extended;
+        }
+
+        private void PlayerNameListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_gameOptions.PlayAgainstComputer)
+                okButton.IsEnabled = (playerNamesListBox.SelectedItems.Count == 1);
+            else
+                okButton.IsEnabled = (playerNamesListBox.SelectedItems.Count == _gameOptions.NumberOfPlayers);
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (string item in playerNamesListBox.SelectedItems)
+            {
+                _gameOptions.SelectedPlayers.Add(item);
+            }
+        }
+
+        private void AddNewPlayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(newPlayerTextBox.Text))
+                _gameOptions.AddPlayer(newPlayerTextBox.Text);
+            newPlayerTextBox.Text = string.Empty;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
